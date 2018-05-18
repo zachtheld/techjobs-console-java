97a744ba46a653315ef4e2edc0f72d431f75efcd
Zach Held <ztheld@gmail.com>
2018-05-17 19:01:34 -0500

    Finished assignment finally

--- src/org/launchcode/techjobs/console/JobData.java
+++ src/org/launchcode/techjobs/console/JobData.java
@@ -76,7 +76,7 @@ public class JobData {
 
             String aValue = row.get(column);
 
-            if (aValue.contains(value)) {
+            if (aValue.toLowerCase().contains(value)) {
                 jobs.add(row);
             }
         }
--- src/org/launchcode/techjobs/console/TechJobs.java
+++ src/org/launchcode/techjobs/console/TechJobs.java
@@ -111,6 +111,7 @@ public class TechJobs {
 
     // Print a list of jobs
     private static void printJobs(ArrayList<HashMap<String, String>> someJobs) {
+        
         if (someJobs.isEmpty()) {
             System.out.println("No Jobs Found");
         }
